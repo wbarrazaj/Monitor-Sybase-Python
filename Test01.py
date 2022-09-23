a10737e93fe9acab7437d3f541107aa23c04b3ab
wbarrazaj <wbarraza@gmail.com>
2022-09-23 15:42:54 -0300

    .

---
+++ Test01.py
@@ -0,0 +1,12 @@
+import sqlanydb
+import os
+
+#print(os.environ["SQLANY_API_DLL"])
+
+
+conn = sqlanydb.connect(uid='sa', pwd='Emilita01', eng='PROD' , commlinks='tcpip{host=193.168.1.175;port=5000}' )
+curs = conn.cursor()
+curs.execute("select 'Hello, world!'")
+print( "SQL Anywhere says: %s" % curs.fetchone() )
+curs.close()
+conn.close()
