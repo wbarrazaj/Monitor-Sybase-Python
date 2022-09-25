68968501af34ad68b77c2d310d603575c44994d8
wbarrazaj <wbarraza@gmail.com>
2022-09-25 02:16:08 -0300

    Clases de Conexiones

--- Test/Test01.py
+++ Test/Test01.py
@@ -12,22 +12,15 @@ curs.close()
 conn.close()
 
 
-
-def print_(mensaje):
-    print(mensaje)
-    print("Hola")
-
-
-print_("Chao")
-
-
-class Jorge() :
-    XX = 1
-    YY = 2 
-    def Gauchon():
-        pass
-
-jomito = Jorge()
-
-print(jomito.XX)
+import pyodbc 
+# Some other example server values are
+# server = 'localhost\sqlexpress' # for a named instance
+# server = 'myserver,port' # to specify an alternate port
+server = 'tcp:myserver.database.windows.net' 
+database = 'mydb' 
+username = 'myusername' 
+password = 'mypassword' 
+# ENCRYPT defaults to yes starting in ODBC Driver 18. It's good to always specify ENCRYPT=yes on the client side to avoid MITM attacks.
+cnxn = pyodbc.connect('DRIVER={ODBC Driver 18 for SQL Server};SERVER='+server+';DATABASE='+database+';ENCRYPT=yes;UID='+username+';PWD='+ password)
+cursor = cnxn.cursor()
 
---
+++ class/Clase_Conexion.py
@@ -0,0 +1,53 @@
+import pyodbc 
+
+
+class Conexion():
+    """
+    Server class de Conexion.
+    print_Server    : Imprime informcion de Servidor.
+    """
+    def __init__(self, servidor, usuario, clave, db, puerto, drver ):
+        self.ServidorDB = servidor 
+        self.UsuarioDB =  usuario
+        self.PasswordDB = clave
+        self.SchemaDBD = db
+        self.Port = puerto 
+        self.Driver = drver
+    
+    def __conectar(self):
+        conn = pyodbc.connect(
+                                driver=self.Driver, 
+                                server=self.ServidorDB , 
+                                database=self.SchemaDBD ,
+                                port = self.Port,
+                                uid=self.UsuarioDB, 
+                                pwd=self.PasswordDB
+                              )
+        return conn
+
+    def ejecutar_query(self, query):
+        conn=self.__conectar()
+        cursor = conn.cursor()
+        cursor.execute(query)
+        conn.commit()
+        conn.close()
+
+
+
+
+""""
+serv = "Server_name"
+usr = "Username" 
+passwd = "Password"
+db = "database_name"
+prt = "port"
+drver="Adaptive Server Enterprise"
+#driver="FreeTDS"
+query="select count (*) from emp"
+print (datetime.datetime.now())
+conn = pyodbc.connect(driver=drver, server=serv, database=db,port = prt,uid=usr, pwd=passwd)
+print(conn)
+cursor = conn.cursor()
+cursor.execute(query)
+row = cursor.fetchall()
+"""
