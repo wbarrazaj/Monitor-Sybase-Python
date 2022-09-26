c28d4d1f128cd2ea521ad35df9ca852917ec2ae6
wbarrazaj <wbarraza@gmail.com>
2022-09-26 11:27:23 -0300

    .

--- Prueba2.py
+++ Prueba2.py
@@ -4,7 +4,7 @@ import os
 #print(os.environ["SQLANY_API_DLL"])
 
 os.environ["SQLANY_API_DLL"]='/opt/sqlanywhere17/lib64/libdbcapi_r.so'
-conn = sqlanydb.connect(uid='sa', pwd='Emilita01', eng='PROD' , commlinks='tcpip{host=193.168.1.175;port=5000}' )
+conn = sqlanydb.connect(uid='sa', pwd='Emilita01', dbn='master' , commlinks='tcpip{host=193.168.1.175;port=5000}' )
 curs = conn.cursor()
 curs.execute("select 'Hello, world!'")
 print( "SQL Anywhere says: %s" % curs.fetchone() )
